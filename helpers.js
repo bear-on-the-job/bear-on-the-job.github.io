6ff8f0cd4b21ff0259bf06df37a53cf89899e06e
bear-on-the-job <87991810+bear-on-the-job@users.noreply.github.com>
2022-05-25 12:57:11 -0400

    Moved work to onload event

--- helpers.js
+++ helpers.js
@@ -1,11 +1,10 @@
 
 var jq = document.createElement('script');
+jq.onload = () => {
+  // ... give time for script to load, then type (or see below for non wait option)
+  jQuery.noConflict();   
+  // Youtube TV click all the free stuff
+  jQuery("div.header:has(div.ytu-onboarding-offer-info:contains('free trial')) tp-yt-paper-checkbox").click()
+}
 jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
 document.getElementsByTagName('head')[0].appendChild(jq);
-// ... give time for script to load, then type (or see below for non wait option)
-jQuery.noConflict();
-
-// Youtube TV click all the free stuff
-jQuery("div.header:has(div.ytu-onboarding-offer-info:contains('free trial')) tp-yt-paper-checkbox").click()
-
-//javascript:(function(e,s){e.src=s;e.onload=function(){jQuery.noConflict();console.log('jQuery injected')};document.head.appendChild(e);})(document.createElement('script'),'//code.jquery.com/jquery-latest.min.js')
